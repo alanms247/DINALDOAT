38431bc7539198f0012a3bc31e8638a5f259b940
alanms247 <alan.miguel@academico.ifpb.edu.br>
2026-04-15 11:16:55 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("IMG_20260408_071723.png")
+st.write("Site Alan")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/alan?usp=sharing")
