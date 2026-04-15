7f19add951bb4952841e18eadbc602c1e212c677
alanms247 <alan.miguel@academico.ifpb.edu.br>
2026-04-15 11:20:04 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
+st.image("teste.jpg")
 st.write("Site Alan")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/alan?usp=sharing")
